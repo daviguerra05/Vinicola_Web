2e826acd4ef6e9a638181e99ac167deedec45dcf
luigiferrarasinno <rm98047@gmail.com.br>
2023-08-20 23:35:17 -0300

    final

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-
 let username = document.getElementById("usuario").value;
 let password = document.getElementById("senha").value;
-
-let senhas_salvas=[];
-let usuarios_salvos=[];
-
+let senhas_salvas = [];
+let usuarios_salvos = [];
 senhas_salvas.push(password);
 usuarios_salvos.push(username);
 
